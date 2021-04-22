fc9676615297b17c98c60c01e300617266378a52
Alice <alicelobinha2@gmail.com>
2021-04-22 16:56:08 -0300

    mofied

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi",
+    { useNewUrlParser: true}
+);
+
+//Primeira rota
 app.get("/", (req, res) => {
     res.send("Hello Rocktseat");
 }
